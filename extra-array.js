22a46107de19819d712e5b40338b594842756373
Svignesh-dev06 <subramaniankv68@gmail.com>
2026-08-31 21:22:24 +0530

    Tasks completed

---
+++ extra-array.js
@@ -0,0 +1,14 @@
+//tasks 1
+
+let array = [1,3,68,9,12,4,23,12];
+let total = 0
+array.forEach(number => {
+
+    total = total+number
+});
+
+
+//tasks 3
+let arr = [3,5,9,7,4,6,8];
+let result = arr.find((num) => num%2==0);
+console.log(result);
---
+++ extra-string.js
@@ -0,0 +1,14 @@
+//tasks 1
+
+let word = "JavaScript is used ot interact with webpages";
+let check = word.includes("JavaScript");
+console.log(check);
+
+//tasks 2
+
+let words = "JavaScript is used to interact with webpages"
+let split = word.split(" ");
+console.log(split);
+
+//tasks 3
+
